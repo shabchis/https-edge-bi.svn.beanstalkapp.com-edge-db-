ac58b825d05bf17edc4a40dd37e6982c18cf6433
shirat <shirat@5490d504-3e4c-4994-8bd8-758a6c579b6b>
2013-04-03 14:41:30 +0000

    Execute Identity I and II as SQL CLR: create common project Metrics.Identity, possibility to execute it in SQl or Debug in .NET

---
+++ EdgeDeliveries/trunk/dbo/Tables/MD_MetricsMetadata.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[MD_MetricsMetadata] (
+    [TableName]     NVARCHAR (100) NOT NULL,
+    [EdgeFieldID]   INT            NULL,
+    [EdgeFieldName] NVARCHAR (50)  NULL,
+    [EdgeTypeID]    INT            NULL,
+    [MeasureName]   NVARCHAR (50)  CONSTRAINT [DF_MD_MetricsMetadata_IsMeasure] DEFAULT ((0)) NULL
+);
+
