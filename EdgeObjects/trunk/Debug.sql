e7278cd2ec978925a568ca13a04c4b519c57b337
shirat <shirat@5490d504-3e4c-4994-8bd8-758a6c579b6b>
2013-04-10 15:00:34 +0000

    Objects Viewer

--- EdgeObjects/trunk/Debug.sql
+++ EdgeObjects/trunk/Debug.sql
@@ -1,2 +1,4 @@
 ﻿--exec dbo.IdentityI
-exec dbo.IdentityI 2, '2__20130408_152316_5f368d7f48490b6484bcc9482b730dba', '2012-01-01', false
+--exec dbo.IdentityI 2, '2__20130408_152316_5f368d7f48490b6484bcc9482b730dba', '2012-01-01', false
+
+exec ObjectViewer 3
