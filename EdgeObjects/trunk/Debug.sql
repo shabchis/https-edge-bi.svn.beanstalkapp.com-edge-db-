a3970a48c5fbe2e50c77c6c87af105fc4926eee0
shirat <shirat@5490d504-3e4c-4994-8bd8-758a6c579b6b>
2013-04-18 07:14:23 +0000

    bug fix in EdgeTypes inheritance

--- EdgeObjects/trunk/Debug.sql
+++ EdgeObjects/trunk/Debug.sql
@@ -1,5 +1,5 @@
 ﻿--exec dbo.IdentityI
 --exec dbo.IdentityI 2, '2__20130408_152316_5f368d7f48490b6484bcc9482b730dba', '2012-01-01', false
 
-exec MD_ObjectsViewer 3
---exec MD_MetricsViewer 3, '[DBO].[3__20130410_201907_5f368d7f48490b6484bcc9482b730dba_Metrics]', a
+--exec MD_ObjectsViewer 3
+exec MD_MetricsViewer 3, '[DBO].[3__20130418_092032_5f368d7f48490b6484bcc9482b730dba_Metrics]', a
